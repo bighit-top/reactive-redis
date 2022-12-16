34bd1fae7cd11e5aac0457919f0ea98402cd488b
topKim <bighit.top@gmail.com>
2022-12-16 19:55:40 +0900

    init project redis pub/sub

---
+++ redis-pubsub/src/main/java/com/redis/redispubsub/RedisPubsubApplication.java
@@ -0,0 +1,13 @@
+package com.redis.redispubsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisPubsubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisPubsubApplication.class, args);
+	}
+
+}
---
+++ redis-pubsub/src/test/java/com/redis/redispubsub/RedisPubsubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redis.redispubsub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisPubsubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
